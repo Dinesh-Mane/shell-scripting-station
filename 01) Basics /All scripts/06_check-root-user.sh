49301e3ab90cd9519f44c258819f6ba1f4bba709
Dinesh Mane <106552945+Dinesh-Mane@users.noreply.github.com>
2025-05-12 13:29:13 +0530

    Create 06_check-root-user.sh

---
+++ 01) Basics /All scripts/06_check-root-user.sh
@@ -0,0 +1,12 @@
+# script to check if a user is root or not.
+# Use Case: Many admin-level scripts should only run as root.
+
+#!/bin/bash
+
+# Check if current user is root
+if [ "$(whoami)" != "root" ]; then
+  echo "You must run this script as root!"
+  exit 1
+fi
+
+echo "You are root. Script running safely."

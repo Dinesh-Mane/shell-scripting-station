f6cf248ea892ad63c0408965beadc3b479a86029
Dinesh Mane <106552945+Dinesh-Mane@users.noreply.github.com>
2025-05-12 17:48:01 +0530

    Update 07_file-exists-check.sh

--- 01) Basics /All scripts/07_file-exists-check.sh
+++ 01) Basics /All scripts/07_file-exists-check.sh
@@ -1,7 +1,7 @@
+#!/bin/bash
+
 # Script to check if a file exists or not.
 # Use Case: Before reading or deleting a file, ensure it exists.
-
-#!/bin/bash
 
 read -p "Enter filename to check: " file
 

5248892607711c622077e6a128d3d4b6e11284f3
Dinesh Mane <106552945+Dinesh-Mane@users.noreply.github.com>
2025-05-12 17:46:22 +0530

    Update 03_interpreter_demo.sh

--- 01) Basics /All scripts/03_interpreter_demo.sh
+++ 01) Basics /All scripts/03_interpreter_demo.sh
@@ -1,7 +1,7 @@
+#!/bin/bash
+
 # Script to Show the Role of an Interpreter
 # Line-by-line execution proves it's interpreted
-
-#!/bin/bash
 
 echo "This script is being interpreted by: $0"  # `$0` shows which shell/interpreter is running the script
 echo "Line 1 executed"

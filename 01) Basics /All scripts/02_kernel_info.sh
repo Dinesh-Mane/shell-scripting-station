e4504039502083628f0a68d85e0f653ebef86105
Dinesh Mane <106552945+Dinesh-Mane@users.noreply.github.com>
2025-05-12 17:46:01 +0530

    Update 02_kernel_info.sh

--- 01) Basics /All scripts/02_kernel_info.sh
+++ 01) Basics /All scripts/02_kernel_info.sh
@@ -1,7 +1,7 @@
+#!/bin/bash
+
 # Script to Understand Kernel Interaction
 # Demonstrates how the shell (via script) interacts with the kernel to get system info
-
-#!/bin/bash
 
 echo "Kernel Name      : $(uname -s)"   # `uname -s` → Shows Kernel name (like Linux)
 echo "Kernel Release   : $(uname -r)"   # uname -r → Shows Kernel release version

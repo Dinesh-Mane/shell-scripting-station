804d56f4b9a2d3d96aefc031f5f36d9937a34ca4
Dinesh Mane <106552945+Dinesh-Mane@users.noreply.github.com>
2025-05-12 13:35:04 +0530

    Create 09_disk-usage-check.sh

---
+++ 01) Basics /All scripts/09_disk-usage-check.sh
@@ -0,0 +1,12 @@
+# Script to display disk usage and warn if it exceeds 80%
+# Use Case: Real-world DevOps/Monitoring scenario
+
+#!/bin/bash
+
+usage=$(df / | grep / | awk '{ print $5 }' | sed 's/%//')  # `df` = disk free & `awk`, `sed` to extract percentage
+
+if [ "$usage" -gt 80 ]; then
+  echo "Warning: Disk usage is above 80%! Currently: $usage%"
+else
+  echo "Disk usage is normal: $usage%"
+fi
